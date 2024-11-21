5e248fe772df18ae2624cd16085f7e2200f7fc9a
server@anyline.org <server@anyline.org>
2024-11-21 15:35:10 +0800

    set

---
+++ src/main/java/org/anyline/office/docx/tag/Set.java
@@ -0,0 +1,61 @@
+package org.anyline.office.docx.tag;
+
+import org.anyline.entity.DataSet;
+import org.anyline.util.BasicUtil;
+import org.anyline.util.BeanUtil;
+import org.anyline.util.regular.RegularUtil;
+
+import java.util.*;
+
+public class Set extends AbstractTag implements Tag{
+
+    private String var;
+    private Object data;
+    private String selector;
+    private Integer index = null;
+    private Integer begin = null;
+    private Integer end = null;
+    private Integer qty = null;
+    public String parse(String text){
+        String html = "";
+        String key = RegularUtil.fetchAttributeValue(text, "data");
+        var = RegularUtil.fetchAttributeValue(text, "var");
+        selector = RegularUtil.fetchAttributeValue(text, "selector");
+        index = BasicUtil.parseInt(RegularUtil.fetchAttributeValue(text, "var"), null);
+        begin = BasicUtil.parseInt(RegularUtil.fetchAttributeValue(text, "begin"), null);
+        end = BasicUtil.parseInt(RegularUtil.fetchAttributeValue(text, "end"), null);
+        qty = BasicUtil.parseInt(RegularUtil.fetchAttributeValue(text, "qty"), null);
+        if(BasicUtil.isEmpty(key) || BasicUtil.isEmpty(var)){
+            return "";
+        }
+        data = data(key);
+        if (null != data) {
+            if(data instanceof Collection) {
+                Collection items = (Collection) data;
+                if(BasicUtil.isNotEmpty(selector)) {
+                    items = BeanUtil.select(items,selector.split(","));
+                }
+                if(index != null) {
+                    int i = 0;
+                    data = null;
+                    for(Object item:items) {
+                        if(index ==i) {
+                            data = item;
+                            break;
+                        }
+                        i ++;
+                    }
+                }else{
+                    int[] range = BasicUtil.range(begin, end, qty, items.size());
+                    if(items instanceof DataSet) {
+                        data = ((DataSet) items).cuts(range[0], range[1]);
+                    }else {
+                        data = BeanUtil.cuts(items, range[0], range[1]);
+                    }
+                }
+            }
+            variables.put(var, data);
+        }
+        return html;
+    }
+}
