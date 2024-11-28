4414ca96debd3a9c4ed7b444673610d6a5f320eb
server@anyline.org <server@anyline.org>
2024-11-28 10:45:09 +0800

    Bookmark

--- src/main/java/org/anyline/office/docx/entity/Bookmark.java
+++ src/main/java/org/anyline/office/docx/entity/Bookmark.java
@@ -2,6 +2,10 @@ package org.anyline.office.docx.entity;
 
 public class Bookmark {
     private String name;
+    public Bookmark(){}
+    public Bookmark(String name){
+        this.name = name;
+    }
 
     public String getName() {
         return name;
