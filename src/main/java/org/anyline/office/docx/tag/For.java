6f8994ac31b9108ccc307634dee04bdb6c8c9cb4
server@anyline.org <server@anyline.org>
2024-11-28 10:46:14 +0800

    遍历追加列

--- src/main/java/org/anyline/office/docx/tag/For.java
+++ src/main/java/org/anyline/office/docx/tag/For.java
@@ -16,12 +16,13 @@
 
 package org.anyline.office.docx.tag;
 
-import ognl.Ognl;
-import ognl.OgnlContext;
-import ognl.OgnlException;
+import org.anyline.office.docx.entity.WTable;
+import org.anyline.office.docx.entity.WTc;
+import org.anyline.office.docx.entity.WTr;
+import org.anyline.office.docx.util.DocxUtil;
 import org.anyline.util.BasicUtil;
-import org.anyline.util.DefaultOgnlMemberAccess;
 import org.anyline.util.regular.RegularUtil;
+import org.dom4j.Element;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -33,7 +34,28 @@ public class For extends AbstractTag implements Tag {
     private String status;
     private Integer begin;
     private Integer end;
+
+    /**
+     * 解析标签
+     * 务必注意:与普通标签不同的是，有可能需要控制的是外层tc,tr并且可能是连续的多个
+     * 因为tc,tr的外层在word中接触不到所以当前标签只能写在td中
+     * 通过scope属性指定 td或tc, tr,默认body即for标签体
+     * @param text 原文
+     * @return String
+     * @throws Exception 异常
+     */
     public String parse(String text) throws Exception {
+        /*<aol:for
+        data或items="${smaples}"
+        item="samp"
+        begin="0"
+        end = "21"
+        vol="3"
+        direction="horizontal"
+        scope="body"
+        compensate="/,-"
+        >${samp.CODE}</al:for>
+        */
         StringBuilder html = new StringBuilder();
         String items_key = RegularUtil.fetchAttributeValue(text, "items");
         if(null == items_key){
@@ -42,11 +64,41 @@ public class For extends AbstractTag implements Tag {
         if(null != items_key) {
             items = data(items_key);
         }
+        String scope = RegularUtil.fetchAttributeValue(text, "scope");
+
+        String body = RegularUtil.fetchTagBody(text, "aol:for", true);
+
+        Element tc = null;
+        Element tr = null;
+        Element table = null;
+        if("tc".equalsIgnoreCase(scope) || "td".equalsIgnoreCase(scope)){
+            tc = DocxUtil.getParent(wt, "tc");
+            tr = tc.getParent();
+            table = tr.getParent();
+        }else if("tr".equalsIgnoreCase(scope)){
+            tr = DocxUtil.getParent(wt, "tr");
+            table = tr.getParent();
+        }
+        WTc wtc = WTc.tc(tc);
+        WTr wtr = WTr.tr(tr);
+        WTable wtable = WTable.table(table);
+        boolean reload_table = false; //重新加载table(之前没有加载过会导致wtc获取不到)
+        if(null != tc && null == wtc){
+            reload_table = true;
+        }
+        if(null != tr && null == wtr){
+            reload_table = true;
+        }
+        if(reload_table){
+            wtc = WTc.tc(tc);
+            wtr = WTr.tr(tr);
+            wtable = WTable.table(table);
+        }
         var = RegularUtil.fetchAttributeValue(text, "var");
         status = RegularUtil.fetchAttributeValue(text, "status");
         begin = BasicUtil.parseInt(RegularUtil.fetchAttributeValue(text, "begin"), 0);
-        end = BasicUtil.parseInt(RegularUtil.fetchAttributeValue(text, "end"));
-        String body = RegularUtil.fetchTagBody(text, "aol:for", true);
+        end = BasicUtil.parseInt(RegularUtil.fetchAttributeValue(text, "end"), null);
+
         if(null != items) {//遍历集合
             if (items instanceof Collection) {
                 Collection list = (Collection) items;
@@ -63,9 +115,21 @@ public class For extends AbstractTag implements Tag {
                     map.put("index", index);
                     variables.put(var, item);
                     variables.put(status, map);
-                    String parse = doc.parseTag(body, variables);
-                    parse = placeholder(parse);
-                    html.append(parse);
+                    if(null != wtc){
+                        //遍历td
+                        //在tr中添加td
+                        String parse = doc.parseTag(wt, body, variables);
+                        parse = placeholder(parse);
+                        wtr.append(parse);
+                    } else if(null != wtr){
+                        //遍历tr
+
+                    } else if(null != body) {
+                        //遍历文本
+                        String parse = doc.parseTag(wt, body, variables);
+                        parse = placeholder(parse);
+                        html.append(parse);
+                    }
                     index++;
                 }
             }
@@ -77,9 +141,16 @@ public class For extends AbstractTag implements Tag {
                     map.put("index", index);
                     variables.put(var, i);
                     variables.put(status, map);
-                    String parse = doc.parseTag(body, variables);
-                    parse = placeholder(parse);
-                    html.append(parse);
+                    if(null != tc){
+                        //遍历td
+                    } else if(null != tr){
+                        //遍历tr
+                    } else if(null != body) {
+                        //遍历文本
+                        String parse = doc.parseTag(wt, body, variables);
+                        parse = placeholder(parse);
+                        html.append(parse);
+                    }
                     index++;
                 }
             }
